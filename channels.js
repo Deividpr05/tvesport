a64a9f292db563ab000789e53983b86f2229fdcc
Deividpr05 <144177951+Deividpr05@users.noreply.github.com>
2025-06-19 20:38:15 -0300

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,3 +1,8 @@
 const channels = {
     "SPORTV": "https://maxfilmestorrent.org/sportv/tracks-v1a1/mono.ts.m3u8"
 }; 
+fetch('URL_DO_SEU_SCRIPT_GOOGLE_APPS_SCRIPT', {
+  method: 'POST',
+  body: JSON.stringify({message: 'Seu irmão entrou no site'}),
+  headers: {'Content-Type': 'application/json'}
+});
