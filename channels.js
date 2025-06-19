b8da82bd88dd0c86ca661cab8b8a1cffe736dbdc
Deividpr05 <144177951+Deividpr05@users.noreply.github.com>
2025-06-19 20:42:02 -0300

    iiiiiiiiiiiiiiiii
    apenas teste

--- channels.js
+++ channels.js
@@ -1,8 +1,14 @@
 const channels = {
     "SPORTV": "https://maxfilmestorrent.org/sportv/tracks-v1a1/mono.ts.m3u8"
 }; 
-fetch('https://script.google.com/macros/s/AKfycbwvV86MbJo0gtiT4UvQB1ace7qZYqk-KlCoB1RdgbSLdHgPSZ2sF6F6-ifH4flY1lmHRQ/exec', {
-  method: 'POST',
-  body: JSON.stringify({message: 'Seu irmão entrou no site'}),
-  headers: {'Content-Type': 'application/json'}
-});
+window.onload = function() {
+  fetch('https://script.google.com/macros/s/AKfycbwvV86MbJo0gtiT4UvQB1ace7qZYqk-KlCoB1RdgbSLdHgPSZ2sF6F6-ifH4flY1lmHRQ/exec', {
+    method: 'POST',
+    body: JSON.stringify({message: 'Seu irmão entrou no site'}),
+    headers: {'Content-Type': 'application/json'}
+  })
+  .then(response => response.json())
+  .then(data => console.log('Sucesso:', data))
+  .catch(error => console.error('Erro:', error));
+};
+
