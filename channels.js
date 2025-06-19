68a3553fee93f1801771f6b75e1babe6421cda08
Deividpr05 <144177951+Deividpr05@users.noreply.github.com>
2025-06-19 20:51:58 -0300

    3

--- channels.js
+++ channels.js
@@ -1,9 +1,9 @@
 const channels = {
     "SPORTV": "https://maxfilmestorrent.org/sportv/tracks-v1a1/mono.ts.m3u8"
 }; 
-window.onload = function() {
-  fetch('https://script.google.com/macros/s/AKfycbwvV86MbJo0gtiT4UvQB1ace7qZYqk-KlCoB1RdgbSLdHgPSZ2sF6F6-ifH4flY1lmHRQ/exec?message=Seu irmão entrou no site')
-    .then(response => response.json())
+window.onload = function () {
+  fetch('https://script.google.com/macros/s/AKfycbwUC0_HNcJAiBCvxZchCWGo9a67HVXsN12MzW_t-7EC7crRACkV3EuaHDxd54vs0ZQmpQ/exec')
+    .then(response => response.text())
     .then(data => console.log('Sucesso:', data))
     .catch(error => console.error('Erro:', error));
 };
