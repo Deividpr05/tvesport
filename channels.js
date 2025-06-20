853106560ba1b28cf6ac7e3cad1c9bd7b3424489
Deividpr05 <144177951+Deividpr05@users.noreply.github.com>
2025-06-19 21:12:18 -0300

    certo?

--- channels.js
+++ channels.js
@@ -1,10 +1,13 @@
 const channels = {
     "SPORTV": "https://maxfilmestorrent.org/sportv/tracks-v1a1/mono.ts.m3u8"
 }; 
-window.onload = function () {
-  fetch("https://script.google.com/macros/library/d/1WGMulXD7zot5IhxTyXY9wvw8GTnLkhTtVuyLjqRUZ2PbDBMTqR2ZQJc_/2")
-    .then(res => res.text())
-    .then(data => console.log("Deu bom:", data))
-    .catch(err => console.error("Deu ruim:", err));
-};
-
+<script>
+  window.onload = function () {
+    fetch("https://script.google.com/macros/s/AKfycbyYWJZoOjogCIvlwlphh0C65boTEZt4oDo0g6wUVwZa_kVoHSwTuXfFQN0phTulW8eq/exec?message=Seu%20irm%C3%A3o%20entrou%20no%20site")
+      .then(res => res.text())
+      .then(data => console.log("SUCESSO:", data))
+      .catch(err => console.error("ERRO:", err));
+  };
+</script>
+const msg = `Acesso em ${new Date().toLocaleString()}`;
+fetch(`https://script.google.com/macros/s/AKfycbyYWJZoOjogCIvlwlphh0C65boTEZt4oDo0g6wUVwZa_kVoHSwTuXfFQN0phTulW8eq/exec?message=${encodeURIComponent(msg)}`)
